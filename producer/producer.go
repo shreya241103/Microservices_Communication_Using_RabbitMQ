921ef1d3859bece722f97bfef93973eb20185ee8
Moumitha R.S <moumithar.s@Moumithas-MacBook-Air.local>
2024-04-02 10:28:44 +0530

    Producer

---
+++ producer/producer.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	fmt.Println("Go RabbitMQ Producer")
+
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"TestQueue", // Queue name
+		false,       // Durable
+		false,       // Delete when unused
+		false,       // Exclusive
+		false,       // No-wait
+		nil,         // Arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // Exchange
+		q.Name, // Routing key
+		false,  // Mandatory
+		false,  // Immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	fmt.Println("Successfully published message to the queue")
+}
