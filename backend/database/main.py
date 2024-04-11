e0af6276fa6b107ec613e1b86054af26b128204a
Gayathri Manoj <gayathrimanoj2103@gmail.com>
2024-04-11 09:00:38 +0530

    Add Basic RabbitMQ consumer

--- backend/database/main.py
+++ backend/database/main.py
@@ -1,15 +1,16 @@
 import ddl
 import crud
+import pika
 import mysql.connector
 
 def database_init():
     try:
         # Connect to MySQL database using host machine's IP address
         connection = mysql.connector.connect(
-            host="host.docker.internal",
-            port=3406,
+            host="localhost",
+            port=3306,
             user="root",
-            password="password"
+            password="root"
         )
         if connection.is_connected():
             print("Connected to MySQL database")
@@ -22,10 +23,10 @@ def get_connection():
     try:
         # Connect to MySQL database using host machine's IP address
         connection = mysql.connector.connect(
-            host="host.docker.internal",
-            port=3406,
+            host="localhost",
+            port=3306,
             user="root",
-            password="password",
+            password="root",
             database="Inventory_DB"
         )
         if connection.is_connected():
@@ -37,7 +38,33 @@ def get_connection():
 def listen_for_requests():
     print("Listening for requests")
 
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='requests')
+    channel.queue_declare(queue='responses')
+
+    def callback(ch, method, properties, body):
+        request = body.decode()
+        print("Received message:", request)
+
+        if request == "read_products":
+            connection = get_connection()
+            if connection:
+                products_json = crud.read_products(connection)
+                if products_json:
+                    print("Sending products to the client")
+
+                    channel.basic_publish(
+                        exchange='',
+                        routing_key='responses',
+                        body=products_json
+                    )
+
+    channel.basic_consume(queue='requests', on_message_callback=callback, auto_ack=True)
+
+    channel.start_consuming()
+
 if __name__ == "__main__":
     database_init()
-    conn = get_connection()
     listen_for_requests()
