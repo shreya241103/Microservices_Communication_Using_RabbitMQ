67bb5eae710dde110f13de6c07d373600b802f0c
Gayathri Manoj <gayathrimanoj2103@gmail.com>
2024-04-20 00:39:11 +0530

    Add Read Orders Function

--- backend/database/crud.py
+++ backend/database/crud.py
@@ -13,8 +13,8 @@ def read_products(connection):
             for product in products:
                 product_dict = {
                     "Product_ID": product[0],
-                    "Product_Name": product[1],
-                    "Product_Description": product[2],
+                    "Name": product[1],
+                    "Description": product[2],
                     "Price": product[3]
                 }
                 products_dict.append(product_dict)
@@ -29,3 +29,35 @@ def read_products(connection):
 
     except Exception as e:
         print("Error Fetching Products:", e)
+
+def read_orders(connection, customer_id):
+    try:
+        if connection.is_connected():
+            cursor = connection.cursor()
+            select_query = f"""SELECT * FROM Order WHERE Customer_ID = {customer_id}
+                            ORDER BY Order_ID;"""
+            cursor.execute(select_query)
+            orders = cursor.fetchall()
+            cursor.close()
+
+            orders_dict = []
+            for order in orders:
+                order_dict = {
+                    "Order_ID": order[0],
+                    "Customer_ID": order[1],
+                    "Product_ID": order[2],
+                    "Quantity": order[3],
+                    "Status": order[4]
+                }
+                orders_dict.append(order_dict)
+
+            # Convert list of dictionaries to JSON
+            orders_json = json.dumps(orders_dict, indent=4)
+
+            print("Orders Fetched:")
+            print(orders_json)
+
+            return orders_json
+
+    except Exception as e:
+        print("Error Fetching Orders:", e)
