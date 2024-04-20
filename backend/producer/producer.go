94e221c5abde49b1ddece92a573dff7bc56ee4a1
Gayathri Manoj <gayathrimanoj2103@gmail.com>
2024-04-20 12:03:32 +0530

    Update Producer Code

--- backend/producer/producer.go
+++ backend/producer/producer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/streadway/amqp"
 )
 
@@ -14,37 +15,38 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-	fmt.Println("Go RabbitMQ Producer")
-
-	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
-	ch, err := conn.Channel()
+	Ch, err = conn.Channel() // Problematic line
 	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
+	defer Ch.Close()
 
-	q, err := ch.QueueDeclare(
-		"TestQueue", // Queue name
-		false,       // Durable
-		false,       // Delete when unused
-		false,       // Exclusive
-		false,       // No-wait
-		nil,         // Arguments
+	// Declare queues
+	_, err = Ch.QueueDeclare(
+		"Read", // name
+		false,  // durable
+		false,  // delete when unused
+		false,  // exclusive
+		false,  // no-wait
+		nil,    // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
-
-	body := "Hello World!"
-	err = ch.Publish(
-		"",     // Exchange
-		q.Name, // Routing key
-		false,  // Mandatory
-		false,  // Immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		})
-	failOnError(err, "Failed to publish a message")
-
-	fmt.Println("Successfully published message to the queue")
+	_, err = Ch.QueueDeclare(
+		"Data", // name
+		false,  // durable
+		false,  // delete when unused
+		false,  // exclusive
+		false,  // no-wait
+		nil,    // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	fmt.Println("RabbitMQ Queues Declared.")
+	go ConsumeDataMessages()
+	router := gin.Default()
+	router.GET("/products", GetProducts)
+	router.GET("/orders:id", GetOrdersByID)
+	router.POST("/order", PostOrder)
+	router.Run("localhost:8080")
 }
