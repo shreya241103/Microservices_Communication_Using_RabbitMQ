0611692333346d42750a44e0e8a5f2ffe5871edb
Moumitha R.S <moumithar.s@Moumithas-MacBook-Air.local>
2024-04-21 00:30:45 +0530

    Add into Restock_Requests

--- backend/stock-management/crud.py
+++ backend/stock-management/crud.py
@@ -20,7 +20,7 @@ def order_status(connection, order):
             # Commit the transaction
             connection.commit()
             if result == 'True':
-                reduce_quantity(connection,product_id,quantity)
+                reduce_quantity(connection,order)
             # Return the result
             return result
 
@@ -28,10 +28,12 @@ def order_status(connection, order):
         print("Error Inserting Order:", e)
         return None
     
-def reduce_quantity(connection, product_id, quantity):
+def reduce_quantity(connection, order):
     try:
         if connection.is_connected():
             # Define the SQL query to update the quantity in the Storage table
+            product_id = order.get("Product_ID", "")
+            quantity = order.get("Quantity", 0)
             query = '''
                 UPDATE Storage
                 SET Quantity = Quantity - %s
@@ -42,10 +44,42 @@ def reduce_quantity(connection, product_id, quantity):
             # Execute the SQL query with parameters
             cursor.execute(query, (quantity, product_id))
 
+            #If threshold greater than quantity
+            cursor.execute("SELECT Quantity, Threshold, Restock_Time FROM Storage WHERE Product_ID = %s", (product_id,))
+            storage_data = cursor.fetchone()
+            current_quantity, threshold, Restock_Time = storage_data
+            if current_quantity < threshold:
+                insert_restock(connection,order,threshold - current_quantity, Restock_Time, "Storage")
             # Commit the transaction
             connection.commit()
             print("Quantity reduced successfully")
 
     except Exception as e:
         print("Error reducing quantity:", e)
+
+def insert_restock(connection, order, quantity, time, type):
+    try:
+        if connection.is_connected():
+            # Extract values from the order dictionary
+            order_id = order.get("Order_ID", "")
+            product_id = order.get("Product_ID", "")
+
+            # Define the SQL query to insert restock request
+            query = """
+                INSERT INTO Restock_Requests 
+                (Product_ID, Type, Order_ID, Date_Time, Status, Quantity) 
+                VALUES (%s, %s, %s, DATE_ADD(NOW(), INTERVAL %s SECOND), %s, %s)
+            """
+            cursor = connection.cursor()
+
+            # Execute the SQL query
+            cursor.execute(query, (product_id, type, order_id, time * quantity, "In Progress", quantity))
+
+            # Commit the transaction
+            connection.commit()
+            print("Restock inserted successfully")
+            
+    except Exception as e:
+        print("Error Inserting Restock:", e)
+
 
